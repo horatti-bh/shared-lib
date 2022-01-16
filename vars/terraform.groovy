6694f69c4397db5142c075a71149cf30c02e7171
CT <ct@gmail.com>
2022-01-16 22:19:19 +0530

    shared lib test

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -13,10 +13,34 @@ def call(Map params = [:]) {
             choice(choices: ['', 'dev', 'prod'], description: 'select env', name: 'ENV')
         }
         stages {
-            stage(Terraform INIT) {
+            stage('Terraform INIT') {
                 when {
                     expression {
                         return INIT
+                    }
+                }
+                steps {
+                    sh '''
+                make init-${ENV}   
+                    '''
+                }
+            }
+            stage('Terraform PLAN') {
+                when {
+                    expression {
+                        return PLAN
+                    }
+                }
+                steps {
+                    sh '''
+                make init-${ENV}   
+                    '''
+                }
+            }
+            stage('Terraform APPLY') {
+                when {
+                    expression {
+                        return APPLY
                     }
                 }
                 steps {
