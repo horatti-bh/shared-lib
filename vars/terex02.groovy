6b9e63cffa66f35c6fde428e91159b8f099beb7b
CT <ct@gmail.com>
2022-01-16 20:15:21 +0530

    shared lib test

---
+++ vars/terex02.groovy
@@ -0,0 +1,25 @@
+def call () {
+    pipeline {
+        agent any
+        stages {
+            stage ('terraform_init_check') {
+                steps {
+                 sh '''
+                   terraform init
+                   '''
+                    }
+                }
+            }
+
+            stage ('terraform_plan_check') {
+                steps {
+                    sh '''
+                        terraform plan
+                        '''
+                    }
+                }
+            }
+        }
+    }
+
+}
