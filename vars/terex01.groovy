1acc69686a95dc16ca0386b5575163255b7bdb69
CT <ct@gmail.com>
2022-01-16 20:09:15 +0530

    shared lib test

--- vars/terex01.groovy
+++ vars/terex01.groovy
@@ -2,3 +2,11 @@ def terraformInit() {
     def execute_state=sh(returnStdout: true, script: "terraform init")
     println execute_state
 }
+
+def terraformplan() {
+    def output=sh(returnStdout: true, script: "terraform plan ")
+}
+
+//def terraformApply() {
+//    def output=sh(returnStdout: true, script: "terraform apply -auto-approve ")
+//}
