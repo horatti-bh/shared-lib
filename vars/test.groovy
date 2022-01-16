6fc6cc028c5f0532825585961b7604139ff593cc
CT <ct@gmail.com>
2022-01-16 17:29:52 +0530

    shared lib test

--- vars/test.groovy
+++ vars/test.groovy
@@ -5,3 +5,18 @@ def info(message) {
 def warnings(message) {
     echo "WARNINGS: ${message}"
 }
+
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage ('test_lib') {
+                steps {
+                 sh '''
+                    echo Hello word
+                    '''
+                }
+            }
+        }
+    }
+}
